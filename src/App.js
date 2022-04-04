4c985ee8377161ef5ff5d918761f9682f1f715a0
RatonRoy <royraton18@gmail.com>
2022-04-04 18:09:02 +0600

    set up components and the routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import About from './About/About';
 import './App.css';
+import Blogs from './Blogs/Blogs';
+import DashBoard from './DashBoard/DashBoard';
+import Header from './Header/Header';
+import Home from './Home/Home';
+import Reviews from './Reviews/Reviews';
 
 function App() {
   return (
     <div className="">
-     
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route> 
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route> 
+        <Route path='/dashboard' element={<DashBoard></DashBoard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element = {<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
